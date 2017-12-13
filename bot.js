9a26bd6021e6ae8ab4b61a970c8280b6d687c3db
RebornX10 <33696653+RebornX10@users.noreply.github.com>
2017-12-13 22:09:18 +0400

    Add files via upload

--- bot.js
+++ bot.js
@@ -13,9 +13,9 @@ client.on('message', message => {
 });
 
 antispam(bot, {
-  warnBuffer: 3, //Maximum amount of messages allowed to send in the interval time before getting warned.
-  maxBuffer: 5, // Maximum amount of messages allowed to send in the interval time before getting banned.
-  interval: 2000, // Amount of time in ms users can send a maximum of the maxBuffer variable before getting banned.
+  warnBuffer: 2, //Maximum amount of messages allowed to send in the interval time before getting warned.
+  maxBuffer: 4, // Maximum amount of messages allowed to send in the interval time before getting banned.
+  interval: 1000, // Amount of time in ms users can send a maximum of the maxBuffer variable before getting banned.
   warningMessage: "Stop le Spam ou c'est un Ban.", // Warning message send to the user indicating they are going to fast.
   banMessage: "a été banni parce qu'il a spammer, d'autres volontaires?", // Ban message, always tags the banned user in front of it.
   maxDuplicatesWarning = 7,// Maximum amount of duplicate messages a user can send in a timespan before getting warned
